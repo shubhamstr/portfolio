92bc7a6cba03c3dfbd626b0582b1730f5759f25d
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-13 21:08:54 +0530

    contact index page update ui

---
+++ js/skills.js
@@ -0,0 +1,17 @@
+let  numberPercent = document.querySelectorAll('.countbar')
+let getPercent = Array.from(numberPercent)
+
+getPercent.map((items) => {
+    let startCount = 0
+    let progressBar = () => {
+        startCount ++
+        items.innerHTML = `<h3>${startCount}%</h3>`
+        items.style.width = `${startCount}%`
+        if(startCount == items.dataset.percentnumber) {
+            clearInterval(stop)
+        }
+    }
+    let stop = setInterval(() => {
+        progressBar()
+    },25)
+})
