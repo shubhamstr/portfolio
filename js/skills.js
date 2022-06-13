0dd410b796ff08f90e085b818d07dc3c1913d899
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-13 22:19:21 +0530

    skills data dynamic with array

--- js/skills.js
+++ js/skills.js
@@ -1,17 +1,53 @@
-let  numberPercent = document.querySelectorAll('.countbar')
-let getPercent = Array.from(numberPercent)
+let numberPercent = document.querySelectorAll(".countbar");
+let getPercent = Array.from(numberPercent);
 
 getPercent.map((items) => {
-    let startCount = 0
-    let progressBar = () => {
-        startCount ++
-        items.innerHTML = `<h3>${startCount}%</h3>`
-        items.style.width = `${startCount}%`
-        if(startCount == items.dataset.percentnumber) {
-            clearInterval(stop)
-        }
+  let startCount = 0;
+  let progressBar = () => {
+    startCount++;
+    items.innerHTML = `<h3>${startCount}%</h3>`;
+    items.style.width = `${startCount}%`;
+    if (startCount == items.dataset.percentnumber) {
+      clearInterval(stop);
     }
-    let stop = setInterval(() => {
-        progressBar()
-    },25)
-})
+  };
+  let stop = setInterval(() => {
+    progressBar();
+  }, 25);
+});
+
+const arrData = [
+  {
+    name: "web design",
+    val: 90,
+  },
+  {
+    name: "web developer",
+    val: 40,
+  },
+  {
+    name: "graphice design",
+    val: 65,
+  },
+  {
+    name: "digital marketing",
+    val: 80,
+  },
+  {
+    name: "javarscript",
+    val: 20,
+  },
+];
+
+var html = "";
+arrData.map((el) => {
+  console.log(el.name, "-", el.val);
+  html +=
+    '<div class="progress"> <div class="title"> <h4>' +
+    el.name +
+    '</h4> </div><div class="barline"> <div class="countbar" data-percentNumber="' +
+    el.val +
+    '"></div></div></div>';
+});
+console.log(html);
+document.getElementById("main").innerHTML = html;
