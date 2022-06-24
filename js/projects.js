68b9e166aeedf0f10ca3757e703ada8ea3f44b63
shubham ubuntu <shubham.sutar@vinnovatetechnologies.com>
2022-06-24 13:22:52 +0530

    projects update

---
+++ js/projects.js
@@ -0,0 +1,55 @@
+let section = document.querySelector('#projects');
+
+let arr = [
+    {
+        projectLink: 'https://first-react-todo-list.herokuapp.com/',
+        projectImage: 'images/todolist.png',
+        projectTitle: 'ToDo List',
+        projectDescription: 'This is react based TODO list. User can add, edit, delete. The tasks are saved in localstorage.'
+    },
+    {
+        projectLink: 'https://first-socketio-chat.herokuapp.com/',
+        projectImage: 'images/chat.png',
+        projectTitle: 'Realtime Chat App',
+        projectDescription: 'This is nodejs Realtime Chatting App. Created using NodeJs, Express, Socket io, etc. Multiple users can chat with together.'
+    }
+]
+
+let title = document.createElement("h3");
+title.innerHTML = "Recent Projects";
+
+section.innerHTML = '';
+section.appendChild(title);
+
+
+let container = document.createElement("div");
+container.classList.add("projects-container");
+
+let markup = ``;
+        arr.map((el) => {
+            markup += `
+            <div class="project-tile">
+                <a target="_blank" href="${el.projectLink}">
+                <img
+                    src="${el.projectImage}"
+                />
+                <p>${el.projectTitle}</p>
+                <small>${el.projectDescription}</small>
+                </a>
+            </div>
+            `;
+        });
+container.innerHTML = markup;
+
+section.appendChild(container);
+
+
+
+
+
+
+
+
+
+
+
