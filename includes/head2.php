e7b7046300d1bfbac1593f3d5780435663839c7d
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-03-11 02:43:09 +0530

    index page redesigned using bootstrap

---
+++ includes/head2.php
@@ -0,0 +1,17 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="assets/images/lappe.ico" type="image/x-icon" />
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet" />
+    <title>Portfolio</title>
+
+
+    <link rel="stylesheet" href="assets/css/fontawsome.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link rel="stylesheet" href="assets/css/main.css" /> -->
+    <link rel="stylesheet" href="assets/css/main2.css" />
+
+
+    <script defer src="web_components/Header.js"></script>
+</head>
---
+++ index2.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'includes/head2.php'?>
+
+<body class="vh-100">
+    <div class="container-fluid px-0 h-100">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container">
+                <a class="navbar-brand" href="#">Logo</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index2.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="skills2.php">Skills</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="projects2.php">Projects</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" aria-current="page" href="contact2.php">Contact</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+
+        <div class="container h-75">
+            <div class="row h-100">
+                <div class="col-10 offset-1 d-flex align-items-center justify-content-center">
+                    <div class="row px-5">
+                        <div class="col-9">
+                            <h2 class="fw-bold">Hello,<br />My name is Shubham Sutar</h2>
+                            <p>
+                                I'm a Full Stack Web Developer lives in Pune, India.<br />
+                                I make Website, usually with JavaScript, PHP, React Js,<br /> Bootstrap. Feel free
+                                to take a look at my latest
+                                <a href="project2.php" class="text-decoration-none">projects.</a><br />connect me at:
+                            </p>
+                            <div class="my-3 social">
+                                <a href="https://github.com/shubhamstr" target="_blank" class="p-1 text-decoration-none">
+                                    <i class="fab fa-github p-2 rounded-circle bg-red"></i>
+                                </a>
+                                <a href="https://codepen.io/shubhamstr" target="_blank" class="p-1 text-decoration-none">
+                                    <i class="fab fa-codepen p-2 rounded-circle"></i>
+                                </a>
+                                <a href="https://twitter.com/shubhamstr_" target="_blank" class="p-1 text-decoration-none">
+                                    <i class="fab fa-twitter p-2 rounded-circle"></i>
+                                </a>
+                                <a href="https://www.linkedin.com/in/shubhamstr" target="_blank" class="p-1 text-decoration-none">
+                                    <i class="fab fa-linkedin-in p-2 rounded-circle"></i>
+                                </a>
+                            </div>
+                            <a class="text-decoration-none btn btn-primary btn-sm"
+                                href="https://drive.google.com/uc?export=download&id=1NQKhFxoJMWBMQonjX0fG4qZktYwjvNtZ7GZQWpIb0-4">Download
+                                My Resume</a>
+                        </div>
+                        <div class="col-3">
+                            <img src="assets/images/lapee.jpg" alt="lapee" class="w-100 lapee_img" />
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <?php include 'includes/scripts2.php'?>
+</body>
+
+</html>
