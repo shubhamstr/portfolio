8c5aeffcfeb62b6cd2d459fce70c0bb86ed52bc5
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-03-11 01:40:10 +0530

    css files merged

--- includes/head.php
+++ includes/head.php
@@ -5,10 +5,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet" />
     <link rel="stylesheet" href="assets/css/fontawsome.css" />
     <link rel="stylesheet" href="assets/css/main.css" />
-    <link rel="stylesheet" href="assets/css/index.css" />
-    <link rel="stylesheet" href="assets/css/skills.css" />
-    <link rel="stylesheet" href="assets/css/projects.css" />
-    <link rel="stylesheet" href="assets/css/contact.css" />
     <title>Portfolio Site</title>
     <script defer src="web_components/Header.js"></script>
 </head>
