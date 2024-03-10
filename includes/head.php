af6c390bec27dc9ba50c924433015a83c6f323a1
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-03-11 01:03:49 +0530

    head component create

---
+++ includes/head.php
@@ -0,0 +1,14 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="shortcut icon" href="assets/images/lappe.ico" type="image/x-icon" />
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href="assets/css/fontawsome.css" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <link rel="stylesheet" href="assets/css/index.css" />
+    <link rel="stylesheet" href="assets/css/skills.css" />
+    <link rel="stylesheet" href="assets/css/projects.css" />
+    <link rel="stylesheet" href="assets/css/contact.css" />
+    <title>Portfolio Site</title>
+    <script defer src="assets/js/Header.js"></script>
+</head>
--- index.php
+++ index.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="shortcut icon" href="assets/images/lappe.ico" type="image/x-icon" />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Lato:wght@700&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="assets/css/fontawsome.css" />
-    <link rel="stylesheet" href="assets/css/main.css" />
-    <link rel="stylesheet" href="assets/css/index.css" />
-    <title>Portfolio Site</title>
-    <script defer src="assets/js/Header.js"></script>
-  </head>
+  <?php include('includes/head.php') ?>
   <body>
     <div class="container">
       <header-ele data-page="index.php"></header-ele>
