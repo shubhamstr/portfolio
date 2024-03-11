04dc8b0cddf6815081b271b3e498df3fba96acdd
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-03-11 23:00:24 +0530

    remove old design files

--- includes/head2.php
+++ includes/head2.php
@@ -9,9 +9,5 @@
     <link rel="stylesheet" href="assets/css/fontawsome.css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- <link rel="stylesheet" href="assets/css/main.css" /> -->
     <link rel="stylesheet" href="assets/css/main2.css" />
-
-
-    <script defer src="web_components/Header.js"></script>
 </head>
