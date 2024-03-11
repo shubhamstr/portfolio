a6068d253fcd5bc4cd36ee0703673ef72e6e71b2
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-03-12 01:07:21 +0530

    logo added, favicon update, skills logic update

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,96 +109,41 @@ if (section) {
 
 // skills page
 
-const arrData = [
-  {
-    name: "HTML",
-    val: 90,
-  },
-  {
-    name: "CSS",
-    val: 90,
-  },
-  {
-    name: "JavaScript",
-    val: 70,
-  },
-  {
-    name: "Bootstrap",
-    val: 70,
-  },
-  {
-    name: "PHP",
-    val: 70,
-  },
-  {
-    name: "Laravel",
-    val: 50,
-  },
-  {
-    name: "React Js",
-    val: 50,
-  },
-  {
-    name: "Node Js",
-    val: 40,
-  },
-  {
-    name: "SQL",
-    val: 80,
-  },
-  {
-    name: "NoSQL",
-    val: 60,
-  },
-  {
-    name: "jQuery",
-    val: 80,
-  },
-  {
-    name: "Git",
-    val: 80,
-  },
-  {
-    name: "SASS",
-    val: 60,
-  },
-  {
-    name: "Jira",
-    val: 60,
-  },
-]
-
-var html = ""
-arrData.map((el) => {
-  //   console.log(el.name, "-", el.val)
-  html +=
-    '<div class="custom_progress"> <div class="title"> <h4>' +
-    el.name +
-    '</h4> </div><div class="barline"> <div class="countbar" data-percentNumber="' +
-    el.val +
-    '"></div></div></div>'
-})
-// console.log(html)
-
-let main = document.getElementById("main")
-if (main) {
-  main.innerHTML = html
-}
-
-let numberPercent = document.querySelectorAll(".countbar")
-let getPercent = Array.from(numberPercent)
-
-getPercent.map((items) => {
-  let startCount = 0
-  let progressBar = () => {
-    startCount++
-    items.innerHTML = `<h3>${startCount}%</h3>`
-    items.style.width = `${startCount}%`
-    if (startCount == items.dataset.percentnumber) {
-      clearInterval(stop)
+fetch("server/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // console.log(data);
+    var html = ""
+    data.skillsData.map((el) => {
+      //   console.log(el.name, "-", el.val)
+      html +=
+        '<div class="custom_progress"> <div class="title"> <h4>' +
+        el.name +
+        '</h4> </div><div class="barline"> <div class="countbar" data-percentNumber="' +
+        el.val +
+        '"></div></div></div>'
+    })
+    // console.log(html)
+    let main = document.getElementById("main")
+    if (main) {
+      main.innerHTML = html
     }
-  }
-  let stop = setInterval(() => {
-    progressBar()
-  }, 10)
-})
+
+    let numberPercent = document.querySelectorAll(".countbar")
+    let getPercent = Array.from(numberPercent)
+
+    getPercent.map((items) => {
+      let startCount = 0
+      let progressBar = () => {
+        startCount++
+        items.innerHTML = `<h3>${startCount}%</h3>`
+        items.style.width = `${startCount}%`
+        if (startCount == items.dataset.percentnumber) {
+          clearInterval(stop)
+        }
+      }
+      let stop = setInterval(() => {
+        progressBar()
+      }, 10)
+    })
+  })
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
-        <a class="navbar-brand" href="#">Portfolio</a>
+        <a class="navbar-brand" href="#">
+            <img src="assets/images/logo.png" alt="" style="width:35px"> Portfolio
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
