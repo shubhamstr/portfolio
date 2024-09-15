a2082b13b415a4a743bc2ed92d379c68066c3615
shubhamvinnovate <shubham.sutar@vinnovatetechnologies.com>
2024-09-15 17:43:23 +0530

    update header

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="assets/images/logo.png" alt="" style="width:35px"> Portfolio
+            <img src="assets/images/logo.png" alt="" style="width:35px">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
